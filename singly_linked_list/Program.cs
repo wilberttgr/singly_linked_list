98518b2300429efbd8c0b3cf7fe9d1a8e54ff5d3
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-11-10 19:01:13 +0700

    Read class Node & declarate variable

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace singly_linked_list
 {
+    class Node
+    {
+        public int noMhs;
+        public string nama;
+        public Node next;
+    }
+   
     internal class Program
     {
         static void Main(string[] args)
