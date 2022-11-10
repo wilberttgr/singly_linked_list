6c3223b3f687151be3c678867aa87daa6ac493bb
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-11-10 19:11:22 +0700

    create method addNode

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -23,8 +23,53 @@ namespace singly_linked_list
         {
             START = null;
         }
+        //Method untuk menambahkan sebuah node ke dalam list
+
+        public void addNote()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            //Node ditambahkan sebagai node pertama 
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diizinkan");
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            //Menemukan lokasi node baru didalam list
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diizinkan");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            //Node baru akan ditempatkan di antara previous dan current
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
     }
-        internal class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
