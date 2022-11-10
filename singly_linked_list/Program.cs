d6b25c0408601c2b13ddb496b8ed980de6e62373
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-11-10 17:02:54 +0700

    Add project files.

---
+++ singly_linked_list/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singly_linked_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
